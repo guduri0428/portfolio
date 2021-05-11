45bd258c0fa446110269f6fd0de4c4e595851c87
GUDURI SRINIVASULU <55395468+guduri0428@users.noreply.github.com>
2021-05-11 12:33:47 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ var myImage=document.getElementById("image")
 var count=0
 function changeImage() {
   let mySrc = myImage.getAttribute('src');
-  var photos=['guduri1.jpeg','guduri2.jpg','guduri3.jpeg','guduri4.jpeg']
+  var photos=['guduri1.jpeg','guduri2.jpg','guduri3.jpeg']
   if (count==photos.length) {
     count=0
   }
